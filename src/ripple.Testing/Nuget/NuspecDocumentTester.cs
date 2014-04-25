f545b249111a8aac8ac176f1667b130edac461e2
Yann ROBIN <me.show@gmail.com>
2014-04-25 21:04:31 +0200

    add test for when no depencies node

---
+++ src/ripple.Testing/Nuget/NuspecDocumentTester.cs
@@ -0,0 +1,48 @@
+﻿using NuGet;
+using NUnit.Framework;
+using ripple.Nuget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FubuTestingSupport;
+using System.Xml.Linq;
+
+namespace ripple.Testing.Nuget
+{
+    public class NuspecDocumentTester
+    {
+        public NuspecDocument createDocument(string fileName, string id, string version)
+        {
+            XNamespace  ns = NuspecDocument.Schema;
+            var xdoc = (new XDocument(new XElement(ns + "package", 
+                new XElement(ns + "metadata",
+                    new XElement(ns + "id",
+                        new XText(id)
+                     ),
+                     new XElement(ns + "version",
+                        new XText(version)
+                     )
+             ))));
+            xdoc.Save(fileName);
+            var doc = new NuspecDocument(fileName);
+            return doc;
+        }
+
+        [Test]
+        public void when_no_depency_add_depency_works()
+        {
+            var fileName = Path.GetTempFileName();
+            var doc = createDocument(fileName, "Test", "1.2.1");
+            doc.AddDependency(new NuspecDependency()
+            {
+                Name = "Test2",
+                VersionSpec = new VersionSpec(SemanticVersion.Parse("1.0.0"))
+            });
+            doc.FindDependencies().ShouldContain(c => c.Name == "Test2");
+
+            File.Delete(fileName);
+        }
+    }
+}
